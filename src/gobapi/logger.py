144dd032baceb1f47174a30970afc79453bc761d
Roel Kramer <contact@roelkramer.nl>
2021-09-28 15:42:30 +0200

    Use Logger library again

--- src/gobapi/logger.py
+++ src/gobapi/logger.py
@@ -6,6 +6,6 @@ from gobcore.logging.logger import Logger
 
 def get_logger(name):
     run_mode = os.getenv('GOB_RUN_MODE', 'PRODUCTION')
-    # if run_mode == 'TEST':
-    return logging.getLogger(name)
-    # return Logger(name)
+    if run_mode == 'TEST':
+        return logging.getLogger(name)
+    return Logger(name)
