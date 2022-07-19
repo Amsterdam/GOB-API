c40df2691dac84e8f45ec07a8c1eb9a3f638fbec
Jaap Koetsier <jaap@chariot-it.nl>
2022-07-19 11:40:05 +0200

    Initialise API (enhanced) views from API instead of Upload

--- src/tests/test_storage.py
+++ src/tests/test_storage.py
@@ -391,6 +391,7 @@ def before_each_storage_test(monkeypatch):
     importlib.reload(gobapi.config)
 
     import gobapi.legacy_views.create
+    import gobapi.views
 
     monkeypatch.setattr(sqlalchemy, 'create_engine', mock_create_engine)
     monkeypatch.setattr(sqlalchemy, 'Table', MockTable)
@@ -405,6 +406,8 @@ def before_each_storage_test(monkeypatch):
     monkeypatch.setattr(gobcore.model.relations, 'get_relation_name', lambda m, a, o, r: 'relation_name')
 
     monkeypatch.setattr(gobapi.legacy_views.create, 'create_legacy_views', lambda x, y: 'create_legacy_views')
+
+    monkeypatch.setattr(gobapi.views, 'initialise_api_views', lambda e: 'initialise_api_views')
 
     import gobapi.storage
     importlib.reload(gobapi.storage)
