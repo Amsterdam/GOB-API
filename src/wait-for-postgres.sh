07eb30fb4eeed8defebad1553f90b71a54acce91
Jaap Koetsier <jaap@chariot-it.nl>
2022-08-10 09:46:49 +0200

    Add timeout to wait-for-postgres script and remove use of .env file in test docker-compose because that don't work

--- src/wait-for-postgres.sh
+++ src/wait-for-postgres.sh
@@ -6,9 +6,18 @@ set -e
 host="$1"
 shift
 
+max=60
+current=0
+
 until PGPASSWORD=$DATABASE_PASSWORD psql -h "$host" -U "$DATABASE_USER" -p $DATABASE_PORT_OVERRIDE -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
+
+  if [ $((current++)) -ge $max ]; then
+    >&2 echo "Timed out waiting for Postgres"
+    exit 1
+  fi
+
 done
 
 >&2 echo "Postgres is up - executing command"
