44f026c293f5cb037c6f3407ea6635cc62819757
Jaap Koetsier <jaap@chariot-it.nl>
2022-08-18 09:27:04 +0200

    Add legacy views for gebieden

--- src/gobapi/legacy_views/create.py
+++ src/gobapi/legacy_views/create.py
@@ -6,6 +6,8 @@ from gobcore.model import GOBModel, relations
 from pydantic import BaseModel
 from sqlalchemy.engine import Engine
 from pathlib import Path
+from psycopg2.errors import InvalidTableDefinition
+from sqlalchemy.exc import ProgrammingError
 
 
 class ViewDefinition(BaseModel):
@@ -17,23 +19,31 @@ class ViewDefinition(BaseModel):
             return self.override_columns[column_name]
 
 
+def _get_view_definitions_path() -> Path:
+    return Path(__file__).parent.joinpath("view_definitions")
+
+
 def _get_tables(model: GOBModel):
     for catalog_name, catalog in model.get_catalogs().items():
         for collection_name, collection in model.get_collections(catalog_name).items():
             yield catalog_name, collection_name, model.get_table_name(catalog_name, collection_name)
 
 
-def _create_or_replace_default_view_query(table_name: str):
-    return f"CREATE OR REPLACE VIEW legacy.{table_name} AS SELECT * FROM public.{table_name}"
+def _default_view_query(table_name: str):
+    return f"SELECT * FROM public.{table_name}"
+
+
+def _open_view_definition(viewdef_path: Path) -> ViewDefinition:
+    with open(viewdef_path, 'r') as f:
+        viewdef = yaml.safe_load(f)
+        return ViewDefinition.parse_obj(viewdef)
 
 
 def _get_custom_view_definition(table_name: str) -> ViewDefinition:
-    viewdef_path = Path(__file__).parent.joinpath(f"view_definitions/{table_name}.yaml")
+    viewdef_path = _get_view_definitions_path().joinpath(f"{table_name}.yaml")
 
     if viewdef_path.exists():
-        with open(viewdef_path, 'r') as f:
-            viewdef = yaml.safe_load(f)
-            return ViewDefinition.parse_obj(viewdef)
+        return _open_view_definition(viewdef_path)
 
 
 def _get_custom_view_query(viewdef: ViewDefinition, model: GOBModel, catalog_name: str, collection_name: str):
@@ -46,12 +56,25 @@ def _get_custom_view_query(viewdef: ViewDefinition, model: GOBModel, catalog_nam
         return field_name
 
     fields = [get_field_def(field) for field in collection['all_fields'].keys()]
-    view_name = model.get_table_name(catalog_name, collection_name)
     from_table_name = viewdef.table_name
 
     fields_formatted = ",\n  ".join(fields)
 
-    return f"CREATE OR REPLACE VIEW legacy.{view_name} AS SELECT\n  {fields_formatted}\nFROM public.{from_table_name}"
+    return f"SELECT\n  {fields_formatted}\nFROM public.{from_table_name}"
+
+
+def _create_view_with_drop_fallback(view_name: str, query: str, connection):
+    create_view_query = f"CREATE OR REPLACE VIEW legacy.{view_name} AS {query}"
+
+    try:
+        connection.execute(create_view_query)
+    except ProgrammingError as e:
+        if isinstance(e.orig, InvalidTableDefinition):
+            # View definition does not match existing view. Drop old and create new
+            connection.execute(f"DROP VIEW legacy.{view_name} CASCADE")
+            connection.execute(create_view_query)
+        else:
+            raise e
 
 
 def _create_views_for_object_tables(model: GOBModel, connection):
@@ -62,9 +85,11 @@ def _create_views_for_object_tables(model: GOBModel, connection):
         if view_definition:
             print(f"Have custom view for {table_name} ({catalog_name} {collection_name})")
             query = _get_custom_view_query(view_definition, model, catalog_name, collection_name)
-            connection.execute(query)
+
+            _create_view_with_drop_fallback(table_name, query, connection)
         else:
-            connection.execute(_create_or_replace_default_view_query(table_name))
+            query = _default_view_query(table_name)
+            _create_view_with_drop_fallback(table_name, query, connection)
 
 
 def _create_views_for_materialized_views(model: GOBModel, connection):
@@ -74,10 +99,11 @@ def _create_views_for_materialized_views(model: GOBModel, connection):
         if view_definition := _get_custom_view_definition(f"rel_{relation_name}"):
             mv_name_in_public = view_definition.table_name.replace("rel_", "mv_", 1)
 
-            query = f"CREATE OR REPLACE VIEW legacy.{mv_name} AS SELECT * FROM public.{mv_name_in_public}"
-            connection.execute(query)
+            query = f"SELECT * FROM public.{mv_name_in_public}"
         else:
-            connection.execute(_create_or_replace_default_view_query(mv_name))
+            query = _default_view_query(mv_name)
+
+        connection.execute(f"CREATE OR REPLACE VIEW legacy.{mv_name} AS {query}")
 
 
 def create_legacy_views(model: GOBModel, engine: Engine):
